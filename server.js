8866e6902dec8220a8dcdc473ef9b74b80256287
Tamin Haloliya <taminhaloliya@gmail.com>
2026-06-24 20:49:07 +0530

    chore: initial server setup

---
+++ server.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const connectDB = require("./src/config/connectDB")
+
+const app = require("./src/app");
+
+connectDB;
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
