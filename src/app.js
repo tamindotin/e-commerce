bdc255376c19353ce8414ccc90cd8cf7cb30ad3e
Tamin Haloliya <taminhaloliya@gmail.com>
2026-06-25 19:09:58 +0530

    feat(): add cookie parser middleware

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const errorHandler = require("./middleware/errorMiddleware");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(errorHandler);
+app.use(cookieParser());
 
 module.exports = app;
