aa8c0c27a2f3816c74bba379daddc613045e11e5
Tamin Haloliya <taminhaloliya@gmail.com>
2026-06-26 22:14:14 +0530

    feat(auth): initialize resend otp controller and route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,10 @@
-const { register, login, verifyAccount } = require("../controller/authController")
+const { register, login, verifyAccount, resendOtp } = require("../controller/authController")
 
 const router = require("express").Router()
 
 router.post("/register", register)
 router.post("/login", login)
 router.post("/verify-account", verifyAccount)
+router.post("/resend-otp", resendOtp)
 
 module.exports = router
