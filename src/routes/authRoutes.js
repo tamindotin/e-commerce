8ec6f132f44243a2ef09696576569756385d9693
Tamin Haloliya <taminhaloliya@gmail.com>
2026-06-26 22:07:02 +0530

    feat(auth): initialize account verification controller and routes and add account verification check

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,9 @@
-const { register, login } = require("../controller/authController")
+const { register, login, verifyAccount } = require("../controller/authController")
 
 const router = require("express").Router()
 
 router.post("/register", register)
 router.post("/login", login)
+router.post("/verify-account", verifyAccount)
 
 module.exports = router
