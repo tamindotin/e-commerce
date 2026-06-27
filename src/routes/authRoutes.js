0bc38e1a035d753b624d3699ae165a91e52e958a
Tamin Haloliya <taminhaloliya@gmail.com>
2026-06-27 13:17:42 +0530

    feat(auth): implement forgot password controller and route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,17 @@
-const { register, login, verifyAccount, resendOtp } = require("../controller/authController")
+const {
+  register,
+  login,
+  verifyAccount,
+  resendOtp,
+  forgotPassword,
+} = require("../controller/authController");
 
-const router = require("express").Router()
+const router = require("express").Router();
 
-router.post("/register", register)
-router.post("/login", login)
-router.post("/verify-account", verifyAccount)
-router.post("/resend-otp", resendOtp)
+router.post("/register", register);
+router.post("/login", login);
+router.post("/verify-account", verifyAccount);
+router.post("/resend-otp", resendOtp);
+router.post("/forgot-password", forgotPassword);
 
-module.exports = router
+module.exports = router;
