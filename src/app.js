63be8ee60ef0fbece6e0f09fc8bacfa4dff31a1e
Tamin Haloliya <taminhaloliya@gmail.com>
2026-06-27 20:34:59 +0530

    feat(user): add user routes to the app

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const errorHandler = require("./middleware/errorMiddleware");
 const cookieParser = require("cookie-parser");
 
 const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
@@ -13,5 +14,6 @@ app.use(errorHandler);
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 module.exports = app;
