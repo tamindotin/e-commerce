f8af8bad7fdfa586a07b7d586ecc3aa2a13c89e5
Tamin Haloliya <taminhaloliya@gmail.com>
2026-06-27 13:15:23 +0530

    refactor(auth): convert send otp method to generic send email method

---
+++ src/helper/sendEmail.js
@@ -0,0 +1,24 @@
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
+
+const sendEmail = async (email, subject, html) => {
+  const info = await transporter.sendMail({
+    from: `From, <${process.env.SENDER}>`,
+    to: email,
+    subject,
+    html,
+  });
+
+  return true;
+};
+
+module.exports = sendEmail;
