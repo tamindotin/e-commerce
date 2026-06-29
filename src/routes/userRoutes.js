b05fdf3696431fb11dbb1534673dcc8e131e668f
Tamin Haloliya <taminhaloliya@gmail.com>
2026-06-29 20:50:26 +0530

    feat(user): add route and validation middleware for updating address

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,16 +3,20 @@ const {
   addAddress,
   setDefaultAddress,
   deleteAddress,
+  updateAddress,
 } = require("../controller/userController");
 const auth = require("../middleware/authMiddleware");
+const validate = require("../middleware/validateMiddleware");
 const {
   addAddressSchema,
+  updateAddressSchema,
 } = require("../validator/userValidator");
 const router = require("express").Router();
 
 router.get("/address", auth, getAllAddresses);
-router.post("/address", auth,addAddressSchema, addAddress);
+router.post("/address", auth, validate(addAddressSchema), addAddress);
 router.patch("/address/:id/default", auth, setDefaultAddress);
 router.delete("/address/:id", auth, deleteAddress);
+router.put("/address/:id", auth, validate(updateAddressSchema), updateAddress);
 
 module.exports = router;
