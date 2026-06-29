dee927cb386614692726bfb7da920959c9600978
Tamin Haloliya <taminhaloliya@gmail.com>
2026-06-29 21:18:41 +0530

    feat(app): add helmet middleware for header security

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const errorHandler = require("./middleware/errorMiddleware");
 const cookieParser = require("cookie-parser");
+const helmet = require("helmet");
 
 const authRoutes = require("./routes/authRoutes");
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+app.use(helmet())
 
 app.use("/api/auth", authRoutes);
 
