daa82cc96150ae306fc6367b22a231518d21dc88
Tamin Haloliya <taminhaloliya@gmail.com>
2026-06-29 20:33:50 +0530

    feat(user): add validation schema to address creation route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,17 @@
-const { getAllAddresses, addAddress, setDefaultAddress, deleteAddress } = require("../controller/userController");
+const {
+  getAllAddresses,
+  addAddress,
+  setDefaultAddress,
+  deleteAddress,
+} = require("../controller/userController");
 const auth = require("../middleware/authMiddleware");
+const {
+  addAddressSchema,
+} = require("../validator/userValidator");
 const router = require("express").Router();
 
 router.get("/address", auth, getAllAddresses);
-router.post("/address", auth, addAddress);
+router.post("/address", auth,addAddressSchema, addAddress);
 router.patch("/address/:id/default", auth, setDefaultAddress);
 router.delete("/address/:id", auth, deleteAddress);
 
