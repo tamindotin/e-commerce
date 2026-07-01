3ff971f6bacb0e8a504ddbd49609ab62455e2b28
Tamin Haloliya <taminhaloliya@gmail.com>
2026-07-01 18:57:37 +0530

    fix(address): make label field required in add and update address schemas

--- src/validator/addressValidator.js
+++ src/validator/addressValidator.js
@@ -25,6 +25,7 @@ const pincode = joi.string().length(6).pattern(/^\d+$/);
 const country = joi.string().min(3).max(15);
 
 const addAddressSchema = joi.object({
+  label: label.required(),
   street: street.required(),
   city: city.required(),
   state: state.required(),
@@ -33,6 +34,7 @@ const addAddressSchema = joi.object({
 });
 
 const updateAddressSchema = joi.object({
+  label: label,
   street: street,
   city: city,
   state: state,
