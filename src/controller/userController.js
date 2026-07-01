a4c9696d82187ba7fc60f4e87ad5def92ffa1964
Tamin Haloliya <taminhaloliya@gmail.com>
2026-07-01 18:58:54 +0530

    feat(user): add user profile retrieval functionality

---
+++ src/controller/userController.js
@@ -0,0 +1,21 @@
+const User = require("../model/userModel");
+const asyncHandler = require("express-async-handler");
+
+const getProfile = asyncHandler(async (req, res) => {
+  const id = req.user;
+
+  const user = await User.findById(id).select("name email verified");
+
+  if (!user) {
+    const error = new Error("user not found. ");
+    error.status = 404;
+    throw error;
+  }
+
+  res.status(200).json({
+    success: true,
+    info: user,
+  });
+});
+
+module.exports = {getProfile}
