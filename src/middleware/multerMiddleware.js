c8b12ceb30142e09506e30a1158d7c81902b4416
Tamin Haloliya <taminhaloliya@gmail.com>
2026-07-04 17:16:33 +0530

    feat(middleware): add multer middleware for image file uploads

---
+++ src/middleware/multerMiddleware.js
@@ -0,0 +1,21 @@
+const multer = require("multer");
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image/")) {
+    cb(null, true);
+  } else {
+    const error = new Error("Only image files are allowed. ");
+    error.status = 400;
+    cb(error, false);
+  }
+};
+
+const upload = multer({
+  dest: "/uploads",
+  fileFilter,
+  limits: {
+    fileSize: 5 * 1024 * 1024,
+  },
+});
+
+module.exports = upload
