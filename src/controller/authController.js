90ea92c49bec5f9392b0949eb026883e963d1679
Tamin Haloliya <taminhaloliya@gmail.com>
2026-07-12 12:38:29 +0530

    feat(auth): add role field in auth token

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -77,7 +77,7 @@ const login = asyncHandler(async (req, res) => {
     throw error;
   }
 
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+  const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
 
