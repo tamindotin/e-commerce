8e7a556b9730c10e06f2158a99d1b0fb2fc3c77a
Tamin Haloliya <taminhaloliya@gmail.com>
2026-07-26 20:16:00 +0530

    feat(auth/api): add verifyUser API endpoint for account verification

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -3,4 +3,5 @@ import api from "./axios";
 export const loginUser = (data) => api.post("/auth/login", data);
 export const registerUser = (data) => api.post("/auth/register", data);
 export const logoutUser = () => api.post("/auth/logout");
-export const getUser = () => api.get("/user")
+export const verifyUser = (data) => api.post("/auth/verify-account", data);
+export const getUser = () => api.get("/user");
