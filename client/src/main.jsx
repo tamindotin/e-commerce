6efae3736f6701ac844630f392c880de9d5297cc
Tamin Haloliya <taminhaloliya@gmail.com>
2026-07-26 11:50:26 +0530

    fix: replace sonner with react-hot-toast for notifications

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,11 +2,11 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
-import { Toaster } from "sonner";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <App />
-    <Toaster richColors position="top-right" />
+    <Toaster/>
   </BrowserRouter>,
 );
