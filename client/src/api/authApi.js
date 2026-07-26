6660dddc1736ed7a7d719008c932a763d971781f
Tamin Haloliya <taminhaloliya@gmail.com>
2026-07-26 16:31:46 +0530

    feat(auth/api): add getUser API endpoint for fetching user data

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -3,3 +3,4 @@ import api from "./axios";
 export const loginUser = (data) => api.post("/auth/login", data);
 export const registerUser = (data) => api.post("/auth/register", data);
 export const logoutUser = () => api.post("/auth/logout");
+export const getUser = () => api.get("/user")
