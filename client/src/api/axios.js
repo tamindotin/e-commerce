b939769c9f14304bae07e0bb24b124310eac234d
Tamin Haloliya <taminhaloliya@gmail.com>
2026-07-29 19:28:28 +0530

    feat(api/axios): add response interceptor for handling 401 errors and token refresh

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -5,4 +5,26 @@ const api = axios.create({
   withCredentials: true
 });
 
+api.interceptors.response.use(
+  (response) => response,
+
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        await api.post("/auth/refresh");
+
+        return api(originalRequest);
+      } catch (err) {
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject(error);
+  },
+);
+
 export default api
